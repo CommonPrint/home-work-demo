923bb7dcfcbc2af5af7d689a3d4cb14d72576eae
Adlet <pioneermove@gmail.com>
2026-05-26 11:10:01 +0600

    Homework #1 completed

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+const currentDate = new Date();
+</script>
+
+<template>
+  <div class="app">
+    <h1>Current Date and Time</h1>
+    <p>{{ currentDate.toLocaleString() }}</p>
+  </div>
+</template>
