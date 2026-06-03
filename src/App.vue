d55591c71a7c5bdf4438387e6233d831114c3bc9
Adlet <pioneermove@gmail.com>
2026-06-04 00:35:13 +0600

    homework #4 completed

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import Card from './components/Card.vue';
+
+const cardData = ref({
+  word: 'car',
+  translation: 'автомобиль',
+  state: 'closed',
+  status: 'success'
+});
+</script>
+
+<template>
+  <Card :data="cardData" />
+</template>
